c0959c19185e9efaa4f2ec26b5f83c532157ba63
Ethan Carollo <etcarollo@gmail.com>
2025-11-14 12:55:26 +0100

    Refactor : update time for animation of screen

--- unity/Assets/Scripts/Utils/ScreenLightUtils.cs
+++ unity/Assets/Scripts/Utils/ScreenLightUtils.cs
@@ -17,7 +17,7 @@ namespace Core.Utils
             {
                 var vector = screenMaterial.GetVector("_TurnWhiteEffect");
                 screenMaterial.SetVector("_TurnWhiteEffect", new Vector2(vector.x, f));
-            }, screenMaterial.GetVector("_TurnWhiteEffect").y, 1f, 1f).setDelay(0.5f).setEaseInOutCirc();
+            }, screenMaterial.GetVector("_TurnWhiteEffect").y, 1f, 0.5f).setDelay(0.2f).setEaseInOutCirc();
         }
 
         public static void AnimateShaderOutInteraction(GameObject gameObject, Material screenMaterial)
@@ -27,13 +27,13 @@ namespace Core.Utils
                 var vector = screenMaterial.GetVector("_TurnWhiteEffect");
                 if (vector.y < 0.05f) vector.y = 0.05f;
                 screenMaterial.SetVector("_TurnWhiteEffect", new Vector2(f, vector.y));
-            }, screenMaterial.GetVector("_TurnWhiteEffect").x, 0f, 0.6f).setDelay(0.5f).setEaseInOutCirc();
+            }, screenMaterial.GetVector("_TurnWhiteEffect").x, 0f, 0.4f).setEaseInOutCirc();
 
             LeanTween.value(gameObject, f =>
             {
                 var vector = screenMaterial.GetVector("_TurnWhiteEffect");
                 screenMaterial.SetVector("_TurnWhiteEffect", new Vector2(vector.x, f));
-            }, screenMaterial.GetVector("_TurnWhiteEffect").y, 0.05f, 0.4f).setDelay(0.5f).setEaseInOutCirc();
+            }, screenMaterial.GetVector("_TurnWhiteEffect").y, 0.05f, 0.3f).setEaseInOutCirc();
         }
     }
 }
