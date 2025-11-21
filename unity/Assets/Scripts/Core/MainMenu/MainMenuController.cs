0b315052fd5ee68b0449b1d120ea121fa9096aa0
Ethan Carollo <etcarollo@gmail.com>
2025-11-21 22:46:50 +0100

    Feat : add parallax in main menu

--- unity/Assets/Scripts/Core/MainMenu/MainMenuController.cs
+++ unity/Assets/Scripts/Core/MainMenu/MainMenuController.cs
@@ -1,3 +1,4 @@
+using System;
 using Core.Scene;
 using Framework.Controller;
 using UnityEngine;
@@ -7,8 +8,22 @@ namespace Core.MainMenu
 {
     public class MainMenuController : BaseController<MainMenuController>
     {
-        public Image maskImage;
-        
+        public RawImage maskImage;
+        public RawImage maskImage2;
+        public RawImage maskImage3;
+        public RawImage maskImage4;
+        public RawImage maskImage5;
+
+        public void Start()
+        {
+            var material = new Material(maskImage.material);
+            maskImage.material = material;
+            maskImage2.material = material;
+            maskImage3.material = material;
+            maskImage4.material = material;
+            maskImage5.material = material;
+        }
+
         public void QuitApplication()
         {
             Application.Quit();
@@ -18,14 +33,16 @@ namespace Core.MainMenu
         
         public void GoOnNewScene()
         {
-            /*
-            LeanTween.moveLocal(maskImage.GetComponent<RectTransform>(), new Vector3(0f, 0f, 0f), 0.3f)
+            LeanTween.value(maskImage.gameObject, (float value) => {
+                maskImage.material.SetFloat("_MaskSize", value);
+            }, maskImage.material.GetFloat("_MaskSize"), 0f, 0.6f).setEaseSpring();
+
+            LeanTween.value(maskImage.gameObject, (Vector3 value) => {
+                maskImage.material.SetVector("_MaskOffset", value);
+            }, maskImage.material.GetVector("_MaskOffset"), new Vector3(0.2f, 0.5f, 0f), 0.6f)
                 .setEaseSpring();
-               LeanTween.delayedCall(0.95f, (_ => { SceneTransitor.Instance.LoadScene(newScene); }));
-               LeanTween.size(maskImage.GetComponent<RectTransform>(), Vector3.zero, 0.8f)
-                   .setEaseSpring();
-                */
-            SceneTransitor.Instance.LoadScene(newScene);
+
+            LeanTween.delayedCall(0.4f, () => { SceneTransitor.Instance.LoadScene(newScene); });
         }
     }
 }
