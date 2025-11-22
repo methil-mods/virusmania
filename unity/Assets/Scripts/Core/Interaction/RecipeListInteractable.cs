cf7dbb0515d121c7ae4779eba497cdef5d8ce66b
Ethan Carollo <etcarollo@gmail.com>
2025-11-22 17:40:41 +0100

    WIP : introducing merge library

---
+++ unity/Assets/Scripts/Core/Interaction/RecipeListInteractable.cs
@@ -0,0 +1,17 @@
+using Core.Player;
+
+namespace Core.Interaction
+{
+    public class RecipeListInteractable : Interactable
+    {
+        public override void Interact(PlayerController playerController)
+        {
+            // Silence...
+        }
+
+        public override void InteractHold(PlayerController playerController)
+        {
+            // Silence...
+        }
+    }
+}
