e39e681971715ffd974f4d99b72705b6297128bd
Ethan Carollo <etcarollo@gmail.com>
2025-11-22 13:17:32 +0100

    WIP : update every typography

--- unity/Assets/Scripts/Core/MainMenu/OptionsController.cs
+++ unity/Assets/Scripts/Core/MainMenu/OptionsController.cs
@@ -6,15 +6,36 @@ namespace Core.MainMenu
     public class OptionsController : BaseController<OptionsController>
     {
         public RectTransform optionPanel;
+        public float duration = 0.25f;
+
+        Vector3 startPos;
+        Vector3 offscreenPos;
+        bool initialized;
+
+        void Init()
+        {
+            if (initialized) return;
+            initialized = true;
+            startPos = optionPanel.localPosition;
+            offscreenPos = startPos + new Vector3(optionPanel.rect.width, 0, 0);
+            optionPanel.localPosition = offscreenPos;
+            optionPanel.gameObject.SetActive(false);
+        }
 
         public void OpenOption()
         {
+            Init();
             optionPanel.gameObject.SetActive(true);
+            LeanTween.moveLocal(optionPanel.gameObject, startPos, duration)
+                .setEaseSpring();
         }
 
         public void CloseOption()
         {
-            optionPanel.gameObject.SetActive(false);
+            Init();
+            LeanTween.moveLocal(optionPanel.gameObject, offscreenPos, duration)
+                .setEaseSpring()
+                .setOnComplete(() => optionPanel.gameObject.SetActive(false));
         }
     }
 }
