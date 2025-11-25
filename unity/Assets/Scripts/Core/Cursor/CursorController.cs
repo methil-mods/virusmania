c609465949b620c2579a1625178d7497a139fb8f
Ethan Carollo <etcarollo@gmail.com>
2025-11-25 19:30:01 +0100

    Refactor : update CursorController

--- unity/Assets/Scripts/Core/Cursor/CursorController.cs
+++ unity/Assets/Scripts/Core/Cursor/CursorController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using Framework.Controller;
 
@@ -13,48 +14,16 @@ namespace Core.Cursor
             base.Awake();
             DontDestroyOnLoad(gameObject);
 
-            var src = CursorDatabase.Instance.cursorTexture;
-            var tex = BilinearScale(src, width, height);
+            var srcTex = CursorDatabase.Instance.cursorTexture;
 
-            UnityEngine.Cursor.SetCursor(tex, CursorDatabase.Instance.hotspot, CursorDatabase.Instance.cursorMode);
-        }
-
-        static Texture2D BilinearScale(Texture2D texture, int w, int h)
-        {
-            var result = new Texture2D(w, h, TextureFormat.ARGB32, false);
-            var px = texture.GetPixels();
-            float xRatio = (texture.width - 1f) / w;
-            float yRatio = (texture.height - 1f) / h;
-
-            for (int y = 0; y < h; y++)
+            try
             {
-                float yy = y * yRatio;
-                int yFloor = (int)yy;
-                int yCeil = Mathf.Min(yFloor + 1, texture.height - 1);
-                float yLerp = yy - yFloor;
-
-                for (int x = 0; x < w; x++)
-                {
-                    float xx = x * xRatio;
-                    int xFloor = (int)xx;
-                    int xCeil = Mathf.Min(xFloor + 1, texture.width - 1);
-                    float xLerp = xx - xFloor;
-
-                    Color bl = px[yFloor * texture.width + xFloor];
-                    Color br = px[yFloor * texture.width + xCeil];
-                    Color tl = px[yCeil * texture.width + xFloor];
-                    Color tr = px[yCeil * texture.width + xCeil];
-
-                    Color top = Color.Lerp(tl, tr, xLerp);
-                    Color bottom = Color.Lerp(bl, br, xLerp);
-                    Color pixel = Color.Lerp(bottom, top, yLerp);
-
-                    result.SetPixel(x, y, pixel);
-                }
+                UnityEngine.Cursor.SetCursor(srcTex, CursorDatabase.Instance.hotspot, CursorDatabase.Instance.cursorMode);
             }
-
-            result.Apply();
-            return result;
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+            }
         }
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
