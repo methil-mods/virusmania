32da36871f03b3a84543c0ccbbfe94024838ddec
Ethan Carollo <etcarollo@gmail.com>
2025-11-27 21:49:49 +0100

    Feat : add ItemHold Interface

--- unity/Assets/Scripts/Core/Item/ItemHoldInterface.cs
+++ unity/Assets/Scripts/Core/Item/ItemHoldInterface.cs
@@ -3,17 +3,26 @@ using Core.Item.Holder;
 using Core.Player;
 using Framework.Controller;
 using Framework.Extensions;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Core.Item
 {
     public class ItemHoldInterface : BaseController<ItemHoldInterface>
     {
+        public GameObject itemHoldInterface;
+        public Image holdItemImage;
+        public TextMeshProUGUI holdItemName;
+        
         public void Start()
         {
             var playerInteraction = PlayerController.Instance.updatables.FirstOfType<PlayerInteraction>();
 
             playerInteraction.OnItemAdded += UpdateInterface;
-            playerInteraction.OnItemRemoved += UpdateInterface;
+            playerInteraction.OnItemRemoved += HideInterface;
+            
+            itemHoldInterface.gameObject.SetActive(false);
         }
 
         public void OnDisable()
@@ -21,12 +30,22 @@ namespace Core.Item
             var playerInteraction = PlayerController.Instance.updatables.FirstOfType<PlayerInteraction>();
             
             playerInteraction.OnItemAdded -= UpdateInterface;
-            playerInteraction.OnItemRemoved -= UpdateInterface;
+            playerInteraction.OnItemRemoved -= HideInterface;
         }
 
-        public void UpdateInterface(HoldItem holdItem)
+        private void HideInterface(HoldItem holdItem)
         {
-            
+            itemHoldInterface.gameObject.SetActive(false);
+        }
+
+        private void UpdateInterface(HoldItem holdItem)
+        {
+            itemHoldInterface.gameObject.SetActive(true);
+            if (holdItem.Item.itemIcon != null)
+            {
+                holdItemImage.sprite = holdItem.Item.itemIcon;
+            }
+            holdItemName.text = holdItem.Item.itemName;
         }
     }
 }
